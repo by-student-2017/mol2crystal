902eab9d9c9169706aebeee032dbba8dade0dbe8
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-09-01 12:53:31 +0900

    Add files via upload

---
+++ postprocess_nwchem.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+
+#---------------------------------------------------------------------------------
+# User setting parameters
+#------------------------------------
+user_precursor_energy_per_atom = 0.0 # [eV] The reference energy (precursor alone) when calculating relative energy.
+#---------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------
+# --- Prepare environment and clean previous results ---
+'''
+# Install libraries
+pip install ase==3.22.1 scipy==1.13.0 psutil==7.0.0
+pip install pymsym==0.3.4
+pip install spglib==2.6.0
+
+# NWChem v7.0.2
+sudo apt update
+sudo apt -y install nwchem-openmpi
+
+# Usage
+pyton3 mol2crystal_nwchem.py
+'''
+#---------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------
+# --- Library imports and warning settings ---
+import os
+import glob
+import shutil
+import numpy as np
+from ase.io import read, write
+#from ase.spacegroup import crystal
+#from scipy.spatial.distance import pdist
+import subprocess
+import psutil
+import re
+from ase.geometry import cellpar_to_cell
+#from ase.neighborlist import NeighborList
+#from ase.data import vdw_radii, atomic_numbers
+
+# NWChem settings
+from ase.units import Ha, Ry
+from ase.calculators.nwchem import NWChem
+from ase.optimize import BFGS, LBFGS, FIRE
+
+# Warning settings
+import warnings
+warnings.filterwarnings("ignore", message="scaled_positions .* are equivalent")
+#---------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------
+# --- Clean old outputs and temporary folders ---
+if (os.path.exists('valid_structures_postprocess_old')):
+    shutil.rmtree( 'valid_structures_postprocess_old')
+
+if (os.path.exists('valid_structures_postprocess')):
+    os.rename(     'valid_structures_postprocess','valid_structures_postprocess_old')
+
+if (os.path.exists('optimized_structures_vasp_old')):
+    shutil.rmtree( 'optimized_structures_vasp_old')   
+
+if (os.path.exists('optimized_structures_vasp')):
+    os.rename(     'optimized_structures_vasp','optimized_structures_vasp_old')
+
+dirs_to_remove = ['temp', 'gaff_pbc_temp', 'reaxff_temp', 'dftb_temp', 'xtb_temp', 'mopac_temp', 
+  'qe_temp', 'abinit_temp', 'openmx_temp', 'gpaw_temp', 'siesta_temp', 'cp2k_temp', 'nwchem_temp', 'elk_temp']
+for dir_name in dirs_to_remove:
+    if os.path.exists(dir_name):
+        shutil.rmtree(dir_name)
+#---------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------
+# --- Configure thread count for OpenMP/OpenMPI --
+cpu_count = psutil.cpu_count(logical=False)
+os.environ["OMP_NUM_THREADS"] = '1'             # use OpenMPI
+#os.environ["OMP_NUM_THREADS"] = str(cpu_count) # use OpenMP 
+#---------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------
+# Output directories
+os.makedirs("optimized_structures_vasp", exist_ok=True)
+os.makedirs("valid_structures_postprocess", exist_ok=True)
+#---------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------
+# NWChem optimization
+def nwchem_optimize(fname, precursor_energy_per_atom):
+    try:
+        temp_dir = "nwchem_temp"
+        if os.path.exists(temp_dir):
+            shutil.rmtree(temp_dir)
+        os.makedirs(temp_dir, exist_ok=True)
+
+        atoms = read(fname)
+        cwd = os.getcwd()
+
+        # ----------------------------------------------------------------------------------------
+        # NWChem calculator settings
+        # ASE, NWChem manual: https://ase-lib.org/ase/calculators/nwchem.html
+        #   Note: tpl = (), lst = [], int = 0, flt = 0.0, str = '' or "", dict = {:}
+        # NWChem: https://nwchemgit.github.io/
+        # ----------------------------------------------------------------------------------------
+        #os.environ["ASE_NWCHEM_COMMAND"] = "/usr/bin/nwchem.openmpi nwchem.nwi > nwchem.nwo"                         # OpenMP  (command is also acceptable)
+        #os.environ["ASE_NWCHEM_COMMAND"] = f"mpirun -np {cpu_count} /usr/bin/nwchem.openmpi nwchem.nwi > nwchem.nwo" # OpenMPI (command is also acceptable)
+        # ----------------------------------------------------------------------------------------
+        calc = NWChem(
+            memory     = str(1024*16)+' mb',   # 1024 * 16 = 16 GB
+            command    = f'mpirun -np {cpu_count} /usr/bin/nwchem.openmpi nwchem.nwi > nwchem.nwo', # NWChem path (ASE_NWCHEM_COMMAND environment variable is also acceptable)
+            label      = 'nwchem',             # Input/output file prefix (Recommend to fix 'nwchem')
+            task       = 'gradient',           # (energy, gradient, optimize, hessian, frequency, dynamics, property, transition)
+            # ----------------------------------------------------------------------------------------
+            #theory     = 'dft',               # (dft, scf, mp2, ccsd, tce, tddft, pspw, band, paw)
+            #basis      = '6-31G*',            # You can change this to another basis set (STO-3G, 3-21G, 6-31G*, cc-pVDZ)
+            # ----------------------------------------------------------------------------------------
+            theory     = 'pspw',               # Ideal for periodic structures. 
+            basis      = 'none',               # Fast and stable with pseudopotential + plane wave.
+            pspw       = {'cutoff': 25.0},     # cutoff energy (Ha unit)
+            # ----------------------------------------------------------------------------------------
+            #theory     = 'paw',               # High accuracy but heavy.
+            #basis      = 'paw',               # 
+            #pspw       = {'cutoff': 25.0},    # cutoff energy (Ha unit)
+            # ----------------------------------------------------------------------------------------
+            xc         = 'PBE',                # Exchange-correlation functional (PBE, B3LYP)
+            dft        = {'convergence': {'energy'  : 1e-3 * len(atoms) / Ha,
+                                          'density' : 1e-5,
+                                          'gradient': 5e-4},
+                          'disp': 'vdw 3',     # DFT-D2:'vdw 2', DFT-D3:'vdw 3'
+                         },                    # NWChem keywords are specified using (potentially nested) dictionaries.
+            #basispar = '',                    # Additional keywords to go in the first line of the basis block.
+            center     = False,                # Do not auto-center geometry
+            autosym    = False,                # Do not auto-symmetrize geometry
+            #symmetry   = '',                  # Leave symmetry unspecified
+            #geompar    = '',                  # Additional keywords to go in the first line of the geometry block.
+            restart_kw = 'start',              # Start a new calculation
+            #set': {'lindep:n_dep': 0}},       # A set of keys and values to be added directly to the NWChem rtdb. 
+            )
+
+        os.chdir(temp_dir)
+        try:
+            atoms.calc = calc
+            #opt = LBFGS(atoms)
+            opt = FIRE(atoms)
+            opt.run(fmax=0.5)
+        finally:
+            os.chdir(cwd)
+
+        # Save optimized structure
+        opt_fname = fname.replace("valid_structures", "optimized_structures_vasp").replace("POSCAR", "OPT") + ".vasp"
+        write(opt_fname, atoms, format='vasp')
+
+        # Energy and density calculations
+        energy_per_atom = atoms.get_potential_energy() / len(atoms)
+        relative_energy_per_atom = energy_per_atom - precursor_energy_per_atom
+
+        total_mass_amu = sum(atoms.get_masses())
+        total_mass_g = total_mass_amu * 1.66053906660e-24
+        volume = atoms.get_volume()
+        volume_cm3 = volume * 1e-24
+        density = total_mass_g / volume_cm3 if volume_cm3 > 0 else 0
+
+        print(f"Final energy per atom: {energy_per_atom:.6f} [eV/atom]")
+        print(f"Final relative energy per atom: {relative_energy_per_atom:.6f} [eV/atom]")
+        print(f"Number of atoms: {len(atoms)}")
+        print(f"Volume: {volume:.6f} [A^3]")
+        print(f"Density: {density:.3f} [g/cm^3]")
+
+        with open("structure_vs_energy.txt", "a") as out:
+            out.write(f"{opt_fname} {relative_energy_per_atom:.6f} {energy_per_atom:.6f} {density:.3f} {len(atoms)} {volume:.6f}\n")
+
+    except Exception as e:
+        print(f"Error optimizing {fname}: {e}")
+#---------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------
+# --- Reference energy from original molecule ---
+with open("structure_vs_energy.txt", "w") as f:
+    print("# POSCAR file, Relative Energy [eV/atom], Total Energy [eV/atom], Density [g/cm^3], Number of atoms, Volume [A^3]", file=f)
+#---------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------
+# --- Main ---
+print(f"------------------------------------------------------")
+print("# Calculate valid structures")
+valid_files = []
+directory = "valid_structures"
+for fname in os.listdir(directory):
+    filepath = os.path.join(directory, fname)
+    try:
+        print(f"Calculate file: {fname}")
+        nwchem_optimize(filepath, precursor_energy_per_atom=user_precursor_energy_per_atom)
+        
+        opt_fname_path  = f"optimized_structures_vasp/{fname}".replace("POSCAR", "OPT") + ".vasp"
+        post_fname_path = f"valid_structures_postprocess/{fname}"
+        shutil.copy(opt_fname_path, post_fname_path)
+        print(f"------------------------------------------------------")
+        continue
+    except Exception:
+        continue
+#---------------------------------------------------------------------------------
+
+print("Finished space group search and Lammps (GAFF) optimization.")
