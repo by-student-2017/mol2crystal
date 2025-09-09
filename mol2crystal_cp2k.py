39a9bd1a9bac71c59d07c4aa53bd841b8a0bd156
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-09-09 09:46:05 +0900

    Add files via upload

--- mol2crystal_cp2k.py
+++ mol2crystal_cp2k.py
@@ -297,7 +297,8 @@ def cp2k_optimize(fname, precursor_energy_per_atom):
             print("cp2k.popt not found in PATH.")
             return
 
-        subprocess.run(["mpirun", "-np", str(cpu_count), "cp2k.popt", "-i", "cp2k.inp", "-o", "cp2k.out"], cwd=temp_dir, check=True)
+        #subprocess.run(["cp2k.popt", "-i", "cp2k.inp", "-o", "cp2k.out"], cwd=temp_dir, check=True)
+        subprocess.run(["mpirun", "-np", str(cpu_count), "cp2k.psmp", "-i", "cp2k.inp", "-o", "cp2k.out"], cwd=temp_dir, check=True)
 
         out_path = os.path.join(temp_dir, "cp2k.out")
         energy_value = None
